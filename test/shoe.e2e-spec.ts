1e5437f680c0d073720cf8ba7b5e736735476483
sl163 <sl163@ya.ru>
2023-09-26 16:18:15 +0300

    Adds tests for shoe delete route;

--- test/shoe.e2e-spec.ts
+++ test/shoe.e2e-spec.ts
@@ -19,9 +19,13 @@ const testShoeDto: ShoeDto = {
   purchaseDate: new Date('1989-07-20'),
 };
 
+const wait = async (time = 1000) =>
+  await new Promise((r) => setTimeout(r, time));
+
 describe('AuthController (e2e)', () => {
   const fakeToken =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NTBjMzE5YTk0NjFkNTRiYzQ3NTFkMDAiLCJ1c2VyRW1haWwiOiJzbDE2M0BoaC5ydSIsImlhdCI6MTY5NTI5Nzk1NiwiZXhwIjoxNjk1Mjk3OTU5fQ.zttsGwenZIWfZt0VHq5XmGTgR8ktWtq2WUa2h7EKtsc';
+  const fakeShoeId = '6512d01295d02ddd8f79101f';
   let app: INestApplication;
   let token: string;
   let shoeId: string;
@@ -43,7 +47,12 @@ describe('AuthController (e2e)', () => {
   }, 10000);
 
   it('/shoe/create (POST) - success', async () => {
-    return request(app.getHttpServer())
+    await request(app.getHttpServer())
+      .get('/user')
+      .set('Authorization', `Bearer ${token}`)
+      .then(({ body }) => expect(body.shoeList).toHaveLength(0));
+
+    await request(app.getHttpServer())
       .post('/shoe/create')
       .set('Authorization', `Bearer ${token}`)
       .send(testShoeDto)
@@ -52,6 +61,12 @@ describe('AuthController (e2e)', () => {
         expect(body._id).toBeDefined();
         shoeId = body._id;
       });
+
+    await wait();
+    return request(app.getHttpServer())
+      .get('/user')
+      .set('Authorization', `Bearer ${token}`)
+      .then(({ body }) => expect(body.shoeList).toHaveLength(1));
   });
 
   it('/shoe/create (POST) - fail (wrong purchase date)', () => {
@@ -88,6 +103,38 @@ describe('AuthController (e2e)', () => {
       .set('Authorization', `Bearer ${fakeToken}`)
       .send({ name: 'new' })
       .expect(401);
+  });
+
+  it('/shoe (DELETE) - fail (wrong shoe owner) ', () => {
+    return request(app.getHttpServer())
+      .delete(`/shoe/${shoeId}`)
+      .set('Authorization', `Bearer ${fakeToken}`)
+      .expect(401);
+  });
+
+  it('/shoe (DELETE) - fail (wrong shoe id) ', () => {
+    return request(app.getHttpServer())
+      .delete(`/shoe/${fakeShoeId}`)
+      .set('Authorization', `Bearer ${token}`)
+      .expect(400);
+  });
+
+  it('/shoe (DELETE) - success', async () => {
+    await request(app.getHttpServer())
+      .get('/user')
+      .set('Authorization', `Bearer ${token}`)
+      .then(({ body }) => expect(body.shoeList).toHaveLength(1));
+
+    await request(app.getHttpServer())
+      .delete(`/shoe/${shoeId}`)
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200);
+
+    await wait();
+    return request(app.getHttpServer())
+      .get('/user')
+      .set('Authorization', `Bearer ${token}`)
+      .then(({ body }) => expect(body.shoeList).toHaveLength(0));
   });
 
   afterAll(() => {
