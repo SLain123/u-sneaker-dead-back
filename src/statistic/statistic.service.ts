9d1805e1da34339aa341ee551e4cf889841e6d25
sl163 <sl163@ya.ru>
2023-10-10 12:05:01 +0300

    Adds new statistics fields in statistic route;

--- src/statistic/statistic.service.ts
+++ src/statistic/statistic.service.ts
@@ -16,7 +16,7 @@ export class StatisticService {
       (acc, { trDistance }) => acc + trDistance,
       0,
     );
-    const avgDistance = sumDistance / allUserRuns.length;
+    const avgDistancePerRun = sumDistance / allUserRuns.length;
 
     const allShoes = await this.shoeService.findAllUserShoes(userId);
     const activeShoes = allShoes.filter(({ active }) => active);
@@ -26,12 +26,25 @@ export class StatisticService {
       0,
     );
 
+    const sortedRuns = allUserRuns.sort(
+      (a: any, b: any) => a.trDate - b.trDate,
+    );
+    const firstRun = sortedRuns[0].trDate;
+    const lastRun = sortedRuns[sortedRuns.length - 1].trDate;
+
+    const sumActiveWeeks = Math.round(
+      (+lastRun - +firstRun) / (1000 * 60 * 60 * 24 * 7),
+    );
+    const avgDistansePerWeek = +(avgDistancePerRun / sumActiveWeeks).toFixed(2);
+
     return {
-      sumDistance, //--------------------- sum of all runs
-      avgDistance, //--------------------- avarage distance by all runs
-      shoesLength: allShoes.length, //---- count of all sneakers
-      activeShoes: activeShoes.length, //- count of active (not broken) sneakers
-      sumActiveDurability, //------------- rest of durability from all active sneakers,
+      sumDistance, //--------------------- sum of all runs in km
+      avgDistancePerRun, //--------------- average distance per one run in km
+      avgDistansePerWeek, //-------------- average distance per week in km
+      sumActiveWeeks, //------------------ count of active time for all time in weeks
+      shoesLength: allShoes.length, //---- count of all sneakers in unit
+      activeShoes: activeShoes.length, //- count of active (not broken) sneakers in unit
+      sumActiveDurability, //------------- rest of durability from all active sneakers in km
     };
   }
 }
